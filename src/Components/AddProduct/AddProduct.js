892c9b8480c5e866573ae724c3fe0cb8e978b4ad
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-04-01 06:33:59 +0600

    implement react hook form

--- src/Components/AddProduct/AddProduct.js
+++ src/Components/AddProduct/AddProduct.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const AddProduct = () => {
+
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <div>
-            
+            <h3>Add Your Product Here</h3>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input name="example" defaultValue="test" ref={register} />
+                <br />
+                <input name="exampleRequired" ref={register({ required: true })} />
+                <br />
+                {errors.exampleRequired && <span>This field is required</span>}
+                <input type="submit" />
+            </form>
         </div>
     );
 };
