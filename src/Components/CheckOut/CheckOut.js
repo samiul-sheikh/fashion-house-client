245e2fa676c649a1df68689fa954e62b1997109f
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-04-04 12:06:18 +0600

    display buy product from database in checkout

--- src/Components/CheckOut/CheckOut.js
+++ src/Components/CheckOut/CheckOut.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { UserContext } from '../../App';
@@ -8,6 +9,14 @@ const CheckOut = () => {
     const { id } = useParams();
     console.log(id);
 
+    const [checkoutProduct, setCheckoutProduct] = useState({});
+
+    useEffect(() => {
+        fetch('http://localhost:8000/product/' + id)
+            .then(res => res.json())
+            .then(data => setCheckoutProduct(data))
+    }, [id])
+
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
 
     return (
@@ -15,6 +24,10 @@ const CheckOut = () => {
         <div style={{ textAlign: 'center' }}>
             <h1>This is Checkout Page</h1>
             <h3> Hello {loggedInUser.name} ! </h3>
+            <img src={checkoutProduct.imageURL} alt=""/>
+            <p>{checkoutProduct.name}</p>
+            <p>{checkoutProduct.price}</p>
+            <button>cheackout</button>
         </div>
     );
 };
