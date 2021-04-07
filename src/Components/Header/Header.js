6c1c4cea4a62bbac1a2aacca3a1c1749e7d144f2
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-04-07 22:26:03 +0600

    design navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,50 +1,26 @@
 import React from 'react';
-import './Header.css';
 import { Link } from "react-router-dom";
-// import {  Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Navbar.Brand to="#home">Fashion House</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    {/* <Nav.Link to="/">Home</Nav.Link> */}
+                </Nav>
 
-            {/* <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-                <Navbar.Brand href="#home">Fashion House</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                    </Nav>
-
-                    <Nav>
-                    <Link to="/">Home</Link>
-                    <Link to="/admin">Admin</Link>
-                    <Link to="/orders">Orders</Link>
-                    <Link to="/login">LogIn</Link>
-                        <Nav.Link href="orders">Orders</Nav.Link>
-                        <Nav.Link href="admin">Admin</Nav.Link>
-                        <Nav.Link href="deals">Deals</Nav.Link>
-                        <Nav.Link eventKey={2} href="login">Login</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar> */}
-
-            <nav className="nav">
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/admin">Admin</Link>
-                    </li>
-                    <li>
-                        <Link to="/orders">Orders</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">LogIn</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+                <Nav>
+                    <Nav.Link to="/" as={Link}>Home</Nav.Link>
+                    <Nav.Link to="/orders" as={Link}>Orders</Nav.Link>
+                    <Nav.Link to="admin" as={Link}>Admin</Nav.Link>
+                    <Nav.Link to="deals" as={Link}>Deals</Nav.Link>
+                    <Nav.Link eventKey={2} to="login" as={Link}>Login</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
 
